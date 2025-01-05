0d62a5923ae4d53e6183482236b701769534f212
uditrocks19 <85439561+uditrocks19@users.noreply.github.com>
2025-01-05 11:42:16 +0530

    added dp approach as well for counting set bits from 0 to n

--- counting_bits.py
+++ counting_bits.py
@@ -11,3 +11,12 @@ class Solution:
                     ans += 1
             res.append(ans)
         return res
+
+# dynamic programming approach
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
